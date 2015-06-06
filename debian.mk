b035ba9eac91164e582293f11f1fdf5c70c86d1c
Aeluin Chen <aeluin.chen@moxa.com>
2015-06-05 19:18:09 -0700

    Adjust the makefile and add release policy.

--- debian.mk
+++ debian.mk
@@ -2,13 +2,16 @@
 include debian.conf
 
 # Where to put executable commands/icons/conf on 'make install'?
-LIBDIR      = $(DESTDIR)/usr/lib/sanji-$(SANJI_VER)/$(RESOURCE)
+LIBDIR      = $(DESTDIR)/usr/lib/sanji-$(SANJI_VER)/$(NAME)
 TMPDIR      = $(DESTDIR)/tmp
 
 FILES       := $(filter-out README.md requirements.txt,$(FILES))
 
 
-all:
+all: $(CURDIR)/packages/bundle-requirements.txt
+	# do nothing
+
+$(CURDIR)/packages/bundle-requirements.txt:
 	mkdir -p $(CURDIR)/packages
 	pip install -r $(CURDIR)/requirements.txt --download \
 		$(CURDIR)/packages || true
